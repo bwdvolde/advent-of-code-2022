7fcd1ad0bcb8817fcfe7e4433536a20d820fc8a3
billie.devolder <billie.devolder@axians.com>
2022-12-04 19:47:44 +0100

    Add day 4

---
+++ day04/solution_part_2.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from read_file.read_file import read_file
+
+
+@dataclass
+class Range:
+    start: int
+    end: int
+
+
+lines = read_file("input.txt")
+
+n_overlapped = 0
+for line in lines:
+    range_unparsed_0, range_unparsed_1 = line.split(",")
+
+
+    def parse(unparsed) -> Range:
+        start, end = unparsed.split("-")
+        start, end = int(start), int(end)
+        return Range(start, end)
+
+
+    range_0, range_1 = parse(range_unparsed_0), parse(range_unparsed_1)
+    if range_0.start > range_1.start or (range_0.start == range_1.start and range_0.end < range_1.end):
+        range_0, range_1 = range_1, range_0
+
+    if range_1.start <= range_0.end:
+        n_overlapped += 1
+
+print(f"Part 2: {n_overlapped}")
