4fc03d76875429dc472e37e16aa81d903f8636bf
Billie Devolder <devolder.billie96@gmail.com>
2022-12-09 08:27:07 +0100

    Add day 5

---
+++ day05/solution_part_2.py
@@ -0,0 +1,55 @@
+import re
+from dataclasses import dataclass
+
+from read_file.read_file import read_file
+
+lines = read_file("input.txt")
+
+parsing_stacks = True
+n_stacks = None
+rows = []
+
+iterator = iter(lines)
+while parsing_stacks:
+    line = next(iterator)
+    row = line[1::4]
+    parsing_stacks = row[0] != "1"
+    if parsing_stacks:
+        rows.append(row)
+    else:
+        n_stacks = len(row)
+
+rows = [f"{{:<{n_stacks}}}".format(row) for row in rows]
+stacks = [[] for _ in range(n_stacks)]
+
+for row in reversed(rows):
+    for i, element in enumerate(row):
+        if element != " ":
+            stacks[i].append(element)
+
+next(iterator)
+
+
+@dataclass
+class Move:
+    amount: int
+    from_stack: int
+    to_stack: int
+
+
+moves = []
+while True:
+    try:
+        line = next(iterator)
+        amount, from_stack, to_stack = re.match("move (.+) from (.+) to (.+)", line).groups()
+        moves.append(Move(int(amount), int(from_stack) - 1, int(to_stack) - 1))
+    except StopIteration:
+        break
+
+for move in moves:
+    to_move = stacks[move.from_stack][-move.amount:]
+    stacks[move.from_stack] = stacks[move.from_stack][:-move.amount]
+    stacks[move.to_stack].extend(to_move)
+
+part_2 = "".join([stack[-1] for stack in stacks])
+print(f"Part 2: {part_2}")
